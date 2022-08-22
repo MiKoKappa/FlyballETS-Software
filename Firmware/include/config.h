c7ae892952145aea21ed3ec5cbef6edebf65bbb0
simonttp78 <simonttp78@gmail.com>
2022-08-22 22:44:08 +0200

    fw version update

--- Firmware/include/config.h
+++ Firmware/include/config.h
@@ -22,7 +22,7 @@
 #ifndef GLOBALVAR_H
 #define GLOBALVAR_H
 
-#define FW_VER "1.6.1"          // Flyball ETS firmware version
+#define FW_VER "2.0.1"          // Flyball ETS firmware version
 
 #define Simulate false          // Set to true to enable race simulation (see Simulator.h/.cpp)
 #define NumSimulatedRaces 47    // Number of prepeared simulated races. Sererial interface command to change interface: e.g. "race 1"
@@ -36,7 +36,6 @@
 #define LIGHTSCHAINS 1          // Numer of WS281x lights chains. 1 - one chain of 5 pixels/lights, 2 - two chains --> 10 pixels/lights, etc.
 #define WS_METHOD NeoEsp32Rmt0800KbpsMethod
 
-#define EEPROM_SIZE 4096        // EEPROM size in bytes
 #define SPI_FLASH_SEC_SIZE 4096 // Flash Sector Size declaration for ESP32 as it seems to become removed from embedded libraries
 
 #define WS_TICKET_BUFFER_SIZE 8 // Number of websocket tickets kept in memory
